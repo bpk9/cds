7292037343a1fc4156bc375877bf8db36c5a04a5
Adrien Zheng <adrien.zheng@coinbase.com>
2025-12-11 15:27:39 -0500

    feat: MediaCard

---
+++ packages/mobile/src/cards/MediaCard/index.tsx
@@ -0,0 +1,56 @@
+import { forwardRef, memo, useCallback, useMemo } from 'react';
+import type { PressableStateCallbackType, StyleProp, View, ViewStyle } from 'react-native';
+import type { ThemeVars } from '@coinbase/cds-common';
+
+import { CardRoot, type CardRootProps } from '../CardRoot';
+
+import { MediaCardLayout, type MediaCardLayoutProps } from './MediaCardLayout';
+
+export type MediaCardBaseProps = MediaCardLayoutProps;
+
+export type MediaCardProps = Omit<CardRootProps, 'children'> &
+  MediaCardBaseProps & {
+    styles?: {
+      root?: StyleProp<ViewStyle> | ((state: PressableStateCallbackType) => StyleProp<ViewStyle>);
+    };
+  };
+
+const mediaCardContainerProps = {
+  borderRadius: 500 as ThemeVars.BorderRadius,
+  background: 'bgAlternate' as ThemeVars.Color,
+  overflow: 'hidden' as const,
+};
+
+export const MediaCard = memo(
+  forwardRef<View, MediaCardProps>(
+    (
+      {
+        title,
+        subtitle,
+        description,
+        thumbnail,
+        media,
+        mediaPlacement = 'end',
+        styles: { root: rootStyle, ...layoutStyles } = {},
+        ...props
+      },
+      ref,
+    ) => {
+      return (
+        <CardRoot ref={ref} {...mediaCardContainerProps} style={rootStyle} {...props}>
+          <MediaCardLayout
+            description={description}
+            media={media}
+            mediaPlacement={mediaPlacement}
+            styles={layoutStyles}
+            subtitle={subtitle}
+            thumbnail={thumbnail}
+            title={title}
+          />
+        </CardRoot>
+      );
+    },
+  ),
+);
+
+MediaCard.displayName = 'MediaCard';
